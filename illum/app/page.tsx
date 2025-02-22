3fed8efd0c97da2c968ebf719ccbf8669cd36dfb
Caleb Krauter <ghostMe66+github@proton.me>
2025-02-21 19:15:40 -0700

    Update to nav and introduce first card module.
    1. Integrate icons for github and linkedin buttons and implement functionality.
    2. Add a rough draft card for a project.

--- illum/app/page.tsx
+++ illum/app/page.tsx
@@ -1,93 +1,27 @@
-import Image from "next/image";
+import Image from 'next/image';
+import StageCard from './components/stage_card';
 
 export default function Home() {
   return (
-    <div className="grid place-items-center p-20">
-      <nav className="bg-gray-800 w-[90vw] mx-auto p-4 flex justify-center items-center">
-        <h1 className=" text-5xl font-bold text-white">
+    <div className='grid place-items-center p-20'>
+      <nav className='bg-gray-800 w-[90vw] mx-auto p-4 flex justify-center items-center'>
+        <h1 className=' text-5xl font-bold text-white'>
           CALEB KRAUTER
         </h1>
-        <div className="pl-6">
-          <button className="bg-gray-900 text-white py-2 px-4 rounded-full hover:bg-red-950"><Image src="/github-mark.png" alt="icon" width={24} height={24} className="mr-1"></Image>
+        <a href='https://github.com/calebkrauter' target='_blank' rel='noopener noreferrer' className='pl-6'>
+          <button className='bg-gray-800 text-white py-2 px-2 rounded-full hover:bg-red-900' >
+            <Image src='/github-mark-white.png' alt='icon' width={24} height={24}></Image>
           </button>
-        </div>
+        </a>
+        <a href='https://www.linkedin.com/in/calebkrauter/' target='_blank' rel='noopener noreferrer' className='pl-6'>
+          <button className='bg-gray-800 text-white py-2 px-2 rounded-full hover:bg-red-900' >
+            <Image src='/In-White-26.png' alt='icon' width={24} height={24}></Image>
+          </button>
+        </a>
       </nav>
-      <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://github.com/calebkrauter"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            My Github
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </div>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <main className='p-10'>
+        <StageCard title='Project 1' thumbnail='/demo.webp'></StageCard>
+      </main>
     </div>
   );
 }
