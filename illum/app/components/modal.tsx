8b2fab258f30bc3a25e3088942007e7b5eb9a576
Caleb Krauter <ghostMe66+github@proton.me>
2025-02-23 22:15:43 -0700

    Update modal.tsx

--- illum/app/components/modal.tsx
+++ illum/app/components/modal.tsx
@@ -11,9 +11,11 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, className }) => {
     if (!isOpen) return null;
 
+    // TODO position and setup the description card. Then add the other 3 card components. Upon setting up each of the four components, consolidate them to the layout component.
+    // Ideally, the layout component will have props where you can pass in the four components that you want in it as props.
     return (
         <div className={`fixed inset-0 bg-white bg-opacity-90 flex items-center justify-center z-10 ${className}`}>
-            // TODO position and setup the description card. Then add the other 3 card components. Upon setting up each of the four components, consolidate them to the layout component.
+
             <DescriptionCard className='bg-gray-700' title='Title' text='description'></DescriptionCard>
 
         </div>
