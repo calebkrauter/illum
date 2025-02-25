d9d6b481d01330042a9412a4a35a39f2583c4135
Caleb Krauter <ghostMe66+github@proton.me>
2025-02-25 15:03:59 -0700

    Update to accessing text card content from calling a fetch in util on json to  using a tsx object to store the data and access it on the client side.

--- illum/app/components/modal.tsx
+++ illum/app/components/modal.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import TextCard from './text_card';
 import ListCard from './list_card';
-import { useConfiguredGlobalCardContent } from '../util';
+// import { useConfiguredGlobalCardContent } from '../util';
+import cardContent from '../card_content';
 import OutsideClickHandler from 'react-outside-click-handler';
 
 interface ModalProps {
@@ -18,9 +19,9 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, className, set
     let descriptionOpacity100: string
     descriptionOpacity100 = 'bg-opacity-100'
     descriptionOpacity0 = 'bg-opacity-0 border-transparent text-transparent'
-    const globalCardContent = useConfiguredGlobalCardContent()
+    // const globalCardContent = useConfiguredGlobalCardContent()
 
-    console.log(globalCardContent); // Check the structure here
+    // console.log(globalCardContent); // Check the structure here
     // TODO Add the other 3 card components. Upon setting up each of the four components, consolidate them to the layout component.
     // Ideally, the layout component will have props where you can pass in the four components that you want in it as props.
     return (
@@ -34,16 +35,16 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, className, set
 <div className="bg-red-500 p-4 outline outline-2 outline-red-500 h-full">Item 3</div>
 <div className="bg-yellow-500 p-4 outline outline-2 outline-yellow-500 h-full">Item 4</div> */}
             {/* FOR testing... */}
-            {
-                globalCardContent.projects.length > 0 && (
-                    <div className={`grid grid-cols-[70%_30%] grid-rows-[30%_70%] borderborder-gray-500 fixed inset-0 items-center justify-center z-10 ${className} ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0'}`}>
-                        <TextCard className={`bg-gray-700 ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0 border-transparent text-transparent'}`} title={globalCardContent.projects[0].content.image.title} text={globalCardContent.projects[0].content.image.path}></TextCard>
-                        <ListCard className={`bg-gray-700 ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0 border-transparent text-transparent'}`} title={globalCardContent.projects[0].content.summary.title} text={globalCardContent.projects[0].content.summary.text || 'No summary available'} delimiter='*'></ListCard>
-                        <TextCard className={`bg-gray-700 ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0 border-transparent text-transparent'}`} title={globalCardContent.projects[0].content.description.title} text={globalCardContent.projects[0].content.description.text}></TextCard>
-                        <ListCard className={`bg-gray-700 ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0 border-transparent text-transparent'}`} title={globalCardContent.projects[0].content.stack.title} text={globalCardContent.projects[0].content.stack.text} delimiter='*'></ListCard>
-                    </div>
-                )
-            }
+            {/* {
+                globalCardContent.projects.length > 0 && ( */}
+            <div className={`grid grid-cols-[70%_30%] grid-rows-[30%_70%] borderborder-gray-500 fixed inset-0 items-center justify-center z-10 ${className} ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0'}`}>
+                <TextCard className={`bg-gray-700 ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0 border-transparent text-transparent'}`} title={cardContent.projects[0].content.image.title} text={cardContent.projects[0].content.image.path}></TextCard>
+                <ListCard className={`bg-gray-700 ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0 border-transparent text-transparent'}`} title={cardContent.projects[0].content.summary.title} text={cardContent.projects[0].content.summary.text} delimiter='*'></ListCard>
+                <TextCard className={`bg-gray-700 ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0 border-transparent text-transparent'}`} title={cardContent.projects[0].content.description.title} text={cardContent.projects[0].content.description.text}></TextCard>
+                <ListCard className={`bg-gray-700 ${isOpen ? 'bg-opacity-100' : 'bg-opacity-0 border-transparent text-transparent'}`} title={cardContent.projects[0].content.stack.title} text={cardContent.projects[0].content.stack.text} delimiter='*'></ListCard>
+            </div>
+            {/* )
+            } */}
 
 
         </OutsideClickHandler>
