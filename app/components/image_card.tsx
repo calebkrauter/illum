7cf5b911e5c219a5faa64d45c1592fbfc737d85a
Caleb Krauter <42187569+calebkrauter@users.noreply.github.com>
2025-03-02 20:01:51 -0700

    Conflict Resolution.

--- app/components/image_card.tsx
+++ app/components/image_card.tsx
@@ -19,12 +19,6 @@ const ImageCard: React.FC<imageCardProps> = ({ title, path, className, xPosition
 
                 <h1 className={`text-xl font-bold p-2 ${className}`}>{title}</h1>
             </div>
-<<<<<<< Updated upstream:illum/app/components/image_card.tsx
-            {/* <div className='absolute inset-0 flex items-center justify-center'>
-                <h2 className='text-3xl font-bold text-white bg-black bg-opacity-30 px-5 py-3 rounded-l transition-all duration-300 group-hover:bg-opacity-0 group-hover:text-opacity-50 select-none'>{title}</h2>
-            </div> */}
-=======
->>>>>>> Stashed changes:app/components/image_card.tsx
         </div >
     )
 }
--- app/components/list_card.tsx
+++ app/components/list_card.tsx
@@ -9,15 +9,6 @@ interface listCardProps {
 
 const ListCard: React.FC<listCardProps> = ({ title, text, className, delimiter = ' ' }) => {
     const listEntry = text.split(delimiter).filter(entry => entry !== '').map((entry, index) => (
-<<<<<<< Updated upstream:illum/app/components/list_card.tsx
-        <li key={index} className=''>{entry}</li>
-    ))
-
-    return (
-        <div className={`p-5 min-h-full max-h-full border rounded-lg shadow-md z-20 ${className}`}  >
-            <h1 className={`text-xl font-bold p-2 ${className}`}>{title}</h1>
-            <div className={`pl-5 ${className}`}>{listEntry}</div>
-=======
         <li key={index} className='' style={{ fontSize: "clamp(.5rem, 1rem, 1.25rem)" }}>{entry}</li>
     ))
 
@@ -26,7 +17,6 @@ const ListCard: React.FC<listCardProps> = ({ title, text, className, delimiter =
             <ul className={`text-lg pl-5 list-disc ${className}`}
             >{listEntry}
             </ul>
->>>>>>> Stashed changes:app/components/list_card.tsx
         </div >
     )
 }
