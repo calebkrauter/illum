257b33e563e709e4fa60511f1392ad2717569df6
Caleb Krauter <42187569+calebkrauter@users.noreply.github.com>
2025-06-24 17:48:43 -0600

    Update mobile dynamics and animations

--- app/components/project-card.tsx
+++ app/components/project-card.tsx
@@ -1,60 +1,74 @@
-import { motion } from 'framer-motion';
-import { Tag } from 'rsuite';
-import Image from 'next/image';
+import { motion } from "framer-motion";
+import { Tag } from "rsuite";
+import Image from "next/image";
 
 function ProjectCard() {
-    return (
-        <a href='https://www.linkedin.com/in/calebkrauter/' target='_blank' rel='noopener noreferrer' className='pl-6'>
-            <motion.div
-                className='group mt-[25px] w-full h-full rounded-lg flex bg-gray-800 hover:bg-white/10 bg-opacity-20'
-                initial={{ x: '100%' }}
-                whileInView={{ x: 0 }}
-                viewport={{ once: true }}
-                transition={{ type: 'spring', stiffness: 200, damping: 20 }}
+  return (
+    <a
+      href="https://www.linkedin.com/in/calebkrauter/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="pl-6">
+      <motion.div
+        className="group mt-[25px] w-full h-full rounded-lg flex bg-gray-800 hover:bg-white/10 bg-opacity-20"
+        initial={{ x: "100%" }}
+        whileInView={{ x: 0 }}
+        viewport={{ once: true }}
+        transition={{ type: "spring", stiffness: 200, damping: 20 }}>
+        <div>
+          <div className="flex sm:flex-row flex-col">
+            <div className="m-[15px] ">
+              <h4 className="group-hover:text-white w-[150px] text-sm font-semibold text-gray-200">
+                2024 Jan - Mar
+              </h4>
+            </div>
 
-            >
-                <div className='m-[15px] '>
-
-                    <h4 className='group-hover:text-white w-[150px] text-sm font-semibold text-gray-200'>
-                        2024 Jan - Mar
-                    </h4>
+            <div className="m-[15px] ">
+              <h4 className="group-hover:text-white mb-[15px] text-md font-semibold text-gray-200">
+                Portfolio Web App
+                <div className="absolute inline">
+                  <button className="ml-[15px] mt-[1px] opacity-35 group-hover:opacity-85 transition-transform duration-300 group-hover:translate-x-[5px] group-hover:translate-y-[-5px]">
+                    <Image
+                      src="/link-arrow.svg"
+                      alt="icon"
+                      width={24}
+                      height={24}></Image>
+                  </button>
                 </div>
-                <div className='m-[15px] flex-col relative'>
+              </h4>
+              <h4 className="group-hover:text-white leading-6 text-sm text-gray-400 font-semibold">
+                A site to display my epic projects.
+              </h4>
 
-                    <h4 className='group-hover:text-white mb-[15px] text-md font-semibold text-gray-200'>
-                        Portfolio Web App
-                        <div className='absolute inline'>
-                            <button className='ml-[15px] mt-[1px] opacity-35 group-hover:opacity-85 transition-transform duration-300 group-hover:translate-x-[5px] group-hover:translate-y-[-5px]' >
-                                <Image src='/link-arrow.svg' alt='icon' width={24} height={24}></Image>
-                            </button>
-
-                        </div>
-
-                    </h4>
-                    <h4 className='group-hover:text-white leading-6 text-sm text-gray-400 font-semibold'>
-                        A site to display my epic projects.
-                    </h4>
-                    <Tag className='group-hover:opacity-100 mt-[15px] opacity-75 transition-transform duration-300 hover:translate-y-[-2px]' color='violet'>
-                        <h4 className='text-sm text-gray-300 font-semibold'>
-                            Next.js
-                        </h4>
-                    </Tag>
-                    <Tag className='group-hover:opacity-100 mt-[15px] opacity-75 transition-transform duration-300 hover:translate-y-[-2px]' color='violet'>
-                        <h4 className='text-sm text-gray-300 font-semibold'>
-                            Typescript
-                        </h4>
-                    </Tag>
-                    <Tag className='group-hover:opacity-100 mt-[15px] opacity-75 transition-transform duration-300 hover:translate-y-[-2px]' color='violet'>
-                        <h4 className='text-sm text-gray-300 font-semibold'>
-                            TailWind
-                        </h4>
-                    </Tag>
-
-                </div>
-
-            </motion.div>
-        </a>
-    )
+              <div className="my-3">
+                <Tag
+                  className="group-hover:opacity-100 mt-[15px] opacity-75 transition-transform duration-300 hover:translate-y-[-2px]"
+                  color="violet">
+                  <h4 className="text-sm text-gray-300 font-semibold">
+                    Next.js
+                  </h4>
+                </Tag>
+                <Tag
+                  className="group-hover:opacity-100 mt-[15px] opacity-75 transition-transform duration-300 hover:translate-y-[-2px]"
+                  color="violet">
+                  <h4 className="text-sm text-gray-300 font-semibold">
+                    Typescript
+                  </h4>
+                </Tag>
+                <Tag
+                  className="group-hover:opacity-100 mt-[15px] opacity-75 transition-transform duration-300 hover:translate-y-[-2px]"
+                  color="violet">
+                  <h4 className="text-sm text-gray-300 font-semibold">
+                    TailWind
+                  </h4>
+                </Tag>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </a>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,87 @@
-'use client'
-import Image from 'next/image';
-import React, { RefObject, useRef } from 'react'
-import 'rsuite/dist/rsuite-no-reset.min.css'
-import 'react-vertical-timeline-component/style.min.css'
-import { motion, useScroll } from 'framer-motion'
-import { Link, Element } from 'react-scroll'
-import ProjectCard from './components/project-card'
-import { useEffect, useState } from 'react'
-import useScrollSpy from 'react-use-scrollspy'
-import ConnectButtons from "./components/connect-buttons"
+"use client";
+import Image from "next/image";
+import React, { RefObject, useRef } from "react";
+import "rsuite/dist/rsuite-no-reset.min.css";
+import "react-vertical-timeline-component/style.min.css";
+import { motion, useScroll } from "framer-motion";
+import { Link, Element } from "react-scroll";
+import ProjectCard from "./components/project-card";
+import { useEffect, useState } from "react";
+import useScrollSpy from "react-use-scrollspy";
+import ConnectButtons from "./components/connect-buttons";
+import { Pivot as Hamburger } from "hamburger-react";
+
 export default function Home() {
-  const [ hasScrolled, setHasScrolled ] = useState(false)
-  const { scrollY } = useScroll()
+  const [hasScrolled, setHasScrolled] = useState(false);
+  const { scrollY } = useScroll();
+  const [isMenuOpen, setMenuOpen] = React.useState(false);
+  const [loaded, setLoaded] = useState(false);
+
   const sectionRefs = [
     useRef<HTMLElement>(null),
     useRef<HTMLElement>(null),
     useRef<HTMLElement>(null),
   ] as React.RefObject<HTMLElement>[];
 
-
   const activeSection = useScrollSpy({
     sectionElementRefs: sectionRefs,
     offsetPx: -120,
-  })
-
+  });
 
   useEffect(() => {
-    const unsubscribe = scrollY.on('change', (y) => {
-      setHasScrolled(y > 50)
-    })
-    return () => unsubscribe()
-  }, [scrollY])
+    const unsubscribe = scrollY.on("change", (y) => {
+      setHasScrolled(y > 50);
+    });
+    return () => unsubscribe();
+  }, [scrollY]);
+
   return (
     <div>
-        <nav className={`fixed left-0 top-0 bg-gray-800 w-full h-[50px] bg-white/10 backdrop-blur-sm transition-opacity duration-400 z-50 opacity-90 rounded-b-lg`}>
-
-          <h1 className='flex text-2xl xfont-bold text-gray-300 ml-[75px] h-full items-center z-50'>
-            CALEB KRAUTER
-          </h1>
-
-          <div className='fixed right-0 top-0 h-[50px] mr-[75px]'>
-            <ConnectButtons/>
+      <nav
+        className={`fixed left-0 top-0 bg-gray-800 w-full h-[50px] bg-white/10 backdrop-blur-sm duration-400 z-50 opacity-90 rounded-b-lg duration-500 ease-in-out ${
+          loaded ? "opacity-100" : "opacity-0"
+        }`}>
+        <div className="flex flex-row h-full">
+          <div className="sm:ml-[75px]vblock sm:hidden">
+            <Hamburger toggled={isMenuOpen} toggle={setMenuOpen} />
           </div>
-        </nav>
+          <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 sm:translate-x-0 sm:left-auto sm:ml-[75px] items-center">
+            <h1 className="sm:text-2xl text-xl font-bold text-gray-300 z-50">
+              CALEB KRAUTER
+            </h1>
+          </div>
+          <div className="fixed right-0 top-0 h-[50px] sm:mr-[75px] mr-[15px]">
+            <div className="flex flex-row gap-5 items-center h-full ml-[75px]">
+              <a
+                href="https://github.com/calebkrauter"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="">
+                <button className="opacity-35 hover:opacity-50">
+                  <Image
+                    src="/github-mark-white.png"
+                    alt="icon"
+                    width={24}
+                    height={24}></Image>
+                </button>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/calebkrauter/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="">
+                <button className="opacity-35 hover:opacity-50">
+                  <Image
+                    src="/ln-white-26.png"
+                    alt="icon"
+                    width={24}
+                    height={24}></Image>
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
       <div>
         <main className="flex justify-center overflow-hidden">
           {/* <div className='sm:fixed sm:top-0 sm:left-0 mt-[100px] sm:w-[400px] w-full sm:h-screen h-[300px] ml-[100px]'>
@@ -97,66 +136,100 @@ export default function Home() {
 
           </div> */}
 
-
-
-          <motion.div className='items-center sm:top-0 sm:right-0 mt-[100px] px-[50px] max-w-[65%] min-w-[500px]'
-            initial={{ x: '100%' }}
+          <motion.div
+            className="items-center sm:top-0 sm:right-0 mt-[5dvh] mx-[15px] sm:mx-[50px] sm:max-w-[70dvw] max-w-[100dvw] min-w-[200px]"
+            initial={{ x: "100%" }}
             whileInView={{ x: 0 }}
             viewport={{ once: true }}
-            transition={{ type: 'spring', stiffness: 250, damping: 20 }}
-          >
-            <div className='overflow-hidden'>
+            transition={{ type: "spring", stiffness: 250, damping: 20 }}>
+            <div className="overflow-hidden">
               <section className="App-section" ref={sectionRefs[0]}>
-
-
-                <Element name={'about'} >
-
-                  <div className='group flex flex-col mt-[50px] mx-auto'>
-                    <div className='flex xl:flex-row flex-col gap-[50px] md:items-center'>
+                <Element name={"about"}>
+                  <div className="group flex flex-col mt-[50px] mx-auto">
+                    <div className="flex xl:flex-row flex-col gap-[50px] md:items-center">
                       <div>
-                        <h4 className='leading-7 text-white'>
-                          My name is Caleb. I'm a Software Developer and part-time barista.{<br/>}{<br/>}
-                          When I'm not at home writing code or at work making coffee, you can often find me spending time with family and friends building relationships where it matters most.
-
-                          I am a Software Developer with professional IT Help Desk experience. Soon after graduating I was given an opportunity at an internship where I now develop dynamic React Components with a great team and a solid mission. 
-                          I've contributed to several projects using JS, TS, React, Java, C, Python, Erlang among other technologies. 
-
-                          I am grateful to the friends who supported me in my college journey and who still support me today.
+                        <h4
+                          className={`leading-7 text-white duration-500 ease-in-out ${
+                            loaded ? "opacity-100" : "opacity-0"
+                          }`}>
+                          My name is Caleb. I'm a Software Developer and
+                          part-time barista.{<br />}
+                          {<br />}
+                          When I'm not at home writing code or at work making
+                          coffee, you can often find me spending time with
+                          family and friends building relationships where it
+                          matters most. I am a Software Developer with
+                          professional IT Help Desk experience. Soon after
+                          graduating I was given an opportunity at an internship
+                          where I now develop dynamic React Components with a
+                          great team and a solid mission. I've contributed to
+                          several projects using JS, TS, React, Java, C, Python,
+                          Erlang among other technologies. I am grateful to the
+                          friends who supported me in my college journey and who
+                          still support me today.
                         </h4>
                       </div>
-                    <div className='relative h-[400px] w-[400px] overflow-hidden rounded-md flex-shrink-0'>
-                      <Image src={'/1.png'} width={400} height={400} alt={'Image of Caleb Krauter'} className='translate-y-[-100px]'></Image>
+                      <div className="relative h-[400px] w-[400px] overflow-hidden rounded-md flex-shrink-0">
+                        <Image
+                          src={"/1.png"}
+                          onLoadingComplete={() => {
+                            setLoaded(true);
+                          }}
+                          width={400}
+                          height={400}
+                          alt={"Image of Caleb Krauter"}
+                          className={`translate-y-[-100px] duration-500 ease-in-out ${
+                            loaded ? "opacity-100" : "opacity-0"
+                          }`}></Image>
+                      </div>
                     </div>
                   </div>
-
-                  </div>
+                  <section className="App-section" ref={sectionRefs[2]}>
+                    <Element
+                      className={`mt-[50px] duration-500 ease-in-out ${
+                        loaded ? "opacity-100" : "opacity-0"
+                      }`}
+                      name="experience">
+                      <h4
+                        className={`text-[20px] ${
+                          activeSection === 2
+                            ? "text-gray-200"
+                            : "text-gray-400"
+                        }`}>
+                        Experience
+                      </h4>
+                      <hr
+                        className={`mt-[15px] ${
+                          activeSection === 2
+                            ? "border-gray-200"
+                            : "border-gray-400"
+                        } `}></hr>
+                    </Element>
+                  </section>
                 </Element>
               </section>
-              <section className="App-section" ref={sectionRefs[1]}>
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
 
-                <Element className='mt-[50px]' name='projects'>
-                  <h4 className={`text-[20px] ${activeSection === 1 ? 'text-gray-200' : 'text-gray-400'}`}>
+              <section className="App-section" ref={sectionRefs[1]}>
+                <Element
+                  className={`mt-[50px] duration-500 ease-in-out ${
+                    loaded ? "opacity-100" : "opacity-0"
+                  }`}
+                  name="projects">
+                  <h4
+                    className={`text-[20px] ${
+                      activeSection === 1 ? "text-gray-200" : "text-gray-400"
+                    }`}>
                     Projects
                   </h4>
-                  <hr className={`mt-[15px] ${activeSection === 1 ? 'border-gray-200' : 'border-gray-400'} `}></hr>
-                </Element>
-              </section>
-
-
-              <ProjectCard />
-              <ProjectCard />
-              <ProjectCard />
-              <ProjectCard />
-              <ProjectCard />
-              <ProjectCard />
-              <ProjectCard />
-              <section className="App-section" ref={sectionRefs[2]}>
-
-                <Element className='mt-[50px]' name='experience'>
-                  <h4 className={`text-[20px] ${activeSection === 2 ? 'text-gray-200' : 'text-gray-400'}`}>
-                    Experience
-                  </h4>
-                  <hr className={`mt-[15px] ${activeSection === 2 ? 'border-gray-200' : 'border-gray-400'} `}></hr>
+                  <hr
+                    className={`mt-[15px] ${
+                      activeSection === 1
+                        ? "border-gray-200"
+                        : "border-gray-400"
+                    } `}></hr>
                 </Element>
               </section>
 
@@ -164,18 +237,17 @@ export default function Home() {
               <ProjectCard />
               <ProjectCard />
 
-              <div className='mt-[150px]'>
-
-              </div>
+              <div className="mt-[150px]"></div>
             </div>
           </motion.div>
-        </main >
-
-
-      </div >
-        <footer className='sm:fixed relative left-0 bottom-0 mb-[50px]'>
-          <ConnectButtons/>
-        </footer>
-    </div >
+        </main>
+      </div>
+      <footer
+        className={`sm:fixed relative left-0 bottom-0 mb-[50px] duration-500 ease-in-out ${
+          loaded ? "opacity-100" : "opacity-0"
+        }`}>
+        <ConnectButtons />
+      </footer>
+    </div>
   );
 }
