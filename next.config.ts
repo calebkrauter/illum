f80ca0e8fd8bc04d4452bf01ee0a9fa655e0f2ae
Caleb Krauter <42187569+calebkrauter@users.noreply.github.com>
2025-11-29 16:14:09 -0700

    feat: release banner contains release information

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+// import type { NextConfig } from 'next'
+// import release from './release.json'
+
+
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   env: {
+//     BRANCH: process.env.GITHUB_REF_ || release.branch,
+//     SHA: process.env.GITHUB_SHA || release.sha,
+//     version: process.env.
+//   },
+// }
+
+// export default nextConfig
